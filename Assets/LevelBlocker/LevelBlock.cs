6dcd6da7b24e0dadd964339bd2e662b8188c6bc8
JMStark <joshuamstark@gmail.com>
2023-07-08 15:37:03 -0700

    Try to setup level block editor serialization correctly.

--- Assets/LevelBlocker/LevelBlock.cs
+++ Assets/LevelBlocker/LevelBlock.cs
@@ -3,42 +3,48 @@ using UnityEditor;
 
 public class LevelBlock : MonoBehaviour
 {
-    public float value = 7.0f;
-
-    public Vector3[] points;
+    public Vector3[] points = new Vector3[] {
+        new Vector3(2f, 0f, 2f),
+        new Vector3(2f, 0f, -2f),
+        new Vector3(-2f, 0f, -2f),
+        new Vector3(-2f, 0f, 2f)
+    };
 }
 
+// Investigate
+// https://docs.unity3d.com/ScriptReference/SerializedProperty.html
+// https://docs.unity3d.com/ScriptReference/ScriptableObject.html 
 [CustomEditor(typeof(LevelBlock))]
 public class LevelBlockEditor : Editor
 {
-    public void Awake() {
-        var t = target as LevelBlock;
-        t.points = new Vector3[] {
-            new Vector3(2f, 0f, 2f),
-            new Vector3(2f, 0f, -2f),
-            new Vector3(-2f, 0f, -2f),
-            new Vector3(-2f, 0f, 2f)
-        };
+    private SerializedProperty points;
+
+    public void OnEnable() {
+        points = serializedObject.FindProperty("points");
     }
 
     public void OnSceneGUI()
     {
+        serializedObject.Update();
+
         var t = target as LevelBlock;
         var tr = t.transform;
         var pos = tr.position;
 
         Handles.color = Color.red;
-        for (int i = 0; i < t.points.Length; i++) {
+        for (int i = 0; i < points.arraySize; i++) {
             EditorGUI.BeginChangeCheck();
-            Vector3 newPointPosition = Handles.Slider2D(pos + t.points[i], tr.up, tr.forward, tr.right, 0.1f, Handles.CubeHandleCap, 1f);
+            Vector3 newPointPosition = Handles.Slider2D(pos + points.GetArrayElementAtIndex(i).vector3Value, tr.up, tr.forward, tr.right, 0.1f, Handles.CubeHandleCap, 1f);
             if (EditorGUI.EndChangeCheck())
             {
                 Undo.RecordObject(t, "Change Point Position");
-                t.points[i] = newPointPosition;
+                points.GetArrayElementAtIndex(i).vector3Value = newPointPosition;
             }
         }
 
         Handles.color = new Color(1f, 0f, 0f, 0.4f);
         Handles.DrawAAConvexPolygon(t.points);
+
+        serializedObject.ApplyModifiedProperties();
     }
 }
