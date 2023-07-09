766732a10a5f45bf32ebea50f997a7fe4f6b484a
JMStark <joshuamstark@gmail.com>
2023-07-09 16:37:42 -0700

    Add split point handles, currently they don't properly create a new point yet.

--- Assets/LevelBlocker/LevelBlock.cs
+++ Assets/LevelBlocker/LevelBlock.cs
@@ -30,17 +30,27 @@ public class LevelBlockEditor : Editor
 
         Vector3[] worldPositionPoints = new Vector3[points.arraySize];
 
-        Handles.color = Color.red;
         for (int i = 0; i < points.arraySize; i++) {
             EditorGUI.BeginChangeCheck();
-            
-            Vector3 newPointPosition = Handles.Slider2D(blockPosition + points.GetArrayElementAtIndex(i).vector3Value, blockTransform.up, blockTransform.forward, blockTransform.right, 0.1f, Handles.CubeHandleCap, 1f);
+                    
+            Handles.color = Color.red;
+            Vector3 newPointPosition = Handles.Slider2D(blockPosition + points.GetArrayElementAtIndex(i).vector3Value, blockTransform.up, blockTransform.forward, blockTransform.right, 0.2f, Handles.CubeHandleCap, 1f);
             worldPositionPoints[i] = newPointPosition;
 
             if (EditorGUI.EndChangeCheck())
             {
                 Undo.RecordObject(block, "Change Point Position");
                 points.GetArrayElementAtIndex(i).vector3Value = newPointPosition - blockPosition;
+            }
+
+            Handles.color = Color.yellow;
+            int nextPointIndex = i < points.arraySize - 1 ? i + 1 : 0;
+            Vector3 splitPointPosition = blockPosition + Vector3.Lerp(points.GetArrayElementAtIndex(nextPointIndex).vector3Value, points.GetArrayElementAtIndex(i).vector3Value, 0.5f);
+            
+            if (Handles.Button(splitPointPosition, Quaternion.identity, 0.2f, 0.2f, Handles.CubeHandleCap)) {
+                Debug.Log("Split");
+                points.InsertArrayElementAtIndex(i);
+                points.GetArrayElementAtIndex(i).vector3Value = splitPointPosition;
             }
         }
 
